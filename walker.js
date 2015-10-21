d35e45c63a1551058582367ad718526d228be85e
Alexandra <akscreven@gmail.com>
2015-10-21 09:45:23 -0400

    walker walks to left

--- walker.js
+++ walker.js
@@ -1,10 +1,28 @@
 // Get the walker image:
+
 var walker = document.getElementById('walker');
 
 // Configure motion params:
 var leftBorder = 0;
 var rightBorder = window.innerWidth - walker.offsetWidth;
-
+var currentX = rightBorder;
 // Have the stick figure start at the right border and start walking left
+walker.style.right = rightBorder + 'px';
+// var walkLeft = walkerPosition + '20px'
+
+setInterval(update, 100)
+function update() {
+  currentX = currentX - 10;
+  if(currentX <= leftBorder){
+    currentX = 0;
+    
+  }
+  walker.style.left = currentX + 'px';
+}
+
+
+
 // When he crosses the left border, have him turn around and start walking right (and vice versa)
+
+
 // Bonus: Make him turn around when you click on him
